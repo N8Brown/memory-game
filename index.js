f94118bfdade8aab7e60b3f70831912fdb82514f
Nathan Brown <34713147+N8Brown@users.noreply.github.com>
2019-02-26 10:25:26 -0500

    Initial Component Test
    Move card structure from HTML to Javascript and render as a Vue component.

--- index.js
+++ index.js
@@ -1,31 +1,83 @@
-const game = new Vue ({
-  el: '#game',
-  data: {
-    isActive: false,
-    clickedID1: 0,
-    clickedID2: 0,
-    easy: [
-      {id: 1, imgFront: '/img/html.svg', altFront: 'HTML Logo', imgBack: '/img/code.svg', altBack: 'Code Brackets'},
-      {id: 2, imgFront: '/img/html.svg', altFront: 'HTML Logo', imgBack: '/img/code.svg', altBack: 'Code Brackets'},
-      {id: 3, imgFront: '/img/css.svg', altFront: 'CSS Logo', imgBack: '/img/code.svg', altBack: 'Code Brackets'},
-      {id: 4, imgFront: '/img/css.svg', altFront: 'CSS Logo', imgBack: '/img/code.svg', altBack: 'Code Brackets'},
-      {id: 5, imgFront: '/img/js.svg', altFront: 'Javascript Logo', imgBack: '/img/code.svg', altBack: 'Code Brackets'},
-      {id: 6, imgFront: '/img/js.svg', altFront: 'Javascript Logo', imgBack: '/img/code.svg', altBack: 'Code Brackets'},
-      {id: 7, imgFront: '/img/php.svg', altFront: 'PHP Logo', imgBack: '/img/code.svg', altBack: 'Code Brackets'},
-      {id: 8, imgFront: '/img/php.svg', altFront: 'PHP Logo', imgBack: '/img/code.svg', altBack: 'Code Brackets'}
-    ]
-  },
-
-  methods: {
-    flipCard: function(){
-      if(this.clickedID1 === 0){
-        this.clickedID1 = this.easy.id;
-      } else {
-        this.clickedID2 = this.easy.id;
-      }
+Vue.component("card-easy", {
+  props: ['card'],
+  data: function(){
+    return{
+    isActive: false
+  }},
+  template: `
+    <div class="card-easy" v-on:click="flipCard">
+      <img class="card-front" v-bind:src="card.imgFront" v-bind:alt="card.altFront" v-bind:class="{flip: !isActive}"/>
+      <img class="card-back" v-bind:src="card.imgBack" v-bind:alt="card.altBack" v-bind:class="{flip: isActive}"/>
+    </div>
+  `,
+  methods:{
+    flipCard: function() {
       this.isActive = !this.isActive;
-      console.log(this.clickedID1, this.clickedID2);
+      console.log("Flip");
     }
   }
 });
 
+const game = new Vue({
+  el: "#game",
+  data: {
+    easy: [
+      {
+        id: 1,
+        imgFront: "/img/html.svg",
+        altFront: "HTML Logo",
+        imgBack: "/img/code.svg",
+        altBack: "Code Brackets"
+      },
+      {
+        id: 2,
+        imgFront: "/img/html.svg",
+        altFront: "HTML Logo",
+        imgBack: "/img/code.svg",
+        altBack: "Code Brackets"
+      },
+      {
+        id: 3,
+        imgFront: "/img/css.svg",
+        altFront: "CSS Logo",
+        imgBack: "/img/code.svg",
+        altBack: "Code Brackets"
+      },
+      {
+        id: 4,
+        imgFront: "/img/css.svg",
+        altFront: "CSS Logo",
+        imgBack: "/img/code.svg",
+        altBack: "Code Brackets"
+      },
+      {
+        id: 5,
+        imgFront: "/img/js.svg",
+        altFront: "Javascript Logo",
+        imgBack: "/img/code.svg",
+        altBack: "Code Brackets"
+      },
+      {
+        id: 6,
+        imgFront: "/img/js.svg",
+        altFront: "Javascript Logo",
+        imgBack: "/img/code.svg",
+        altBack: "Code Brackets"
+      },
+      {
+        id: 7,
+        imgFront: "/img/php.svg",
+        altFront: "PHP Logo",
+        imgBack: "/img/code.svg",
+        altBack: "Code Brackets"
+      },
+      {
+        id: 8,
+        imgFront: "/img/php.svg",
+        altFront: "PHP Logo",
+        imgBack: "/img/code.svg",
+        altBack: "Code Brackets"
+      }
+    ]
+  },
+});
